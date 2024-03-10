873902f1cbc72b285465946078b6cbb7dbde1fed
Nick3030w <niconianessu@gmail.com>
2024-03-10 01:59:23 -0500

    se soluciono el ejercicio de triangulo con sus respectivas pruebas

--- src/triangulo.py
+++ src/triangulo.py
@@ -1,6 +1,16 @@
 def evaluar(a, b, c):
-    # TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-    return ""
+    tipo_de_triangulo=""
+    if ((a + b > c) & (a + c > b) & (b + c > a)):
+        if(a == b & b == c):
+            tipo_de_triangulo = "El triángulo es equilátero"
+        elif(a == b | a == c | b == c):
+            tipo_de_triangulo = "El triángulo es isósceles"
+        else:
+            tipo_de_triangulo = "El triángulo es escaleno"
+    else:
+        tipo_de_triangulo = "No es un triángulo válido"        
+
+    return tipo_de_triangulo
 
 if __name__ == '__main__':
     print("a:", end="")
--- tests/test.triangulo.py
+++ tests/test.triangulo.py
@@ -17,8 +17,18 @@ class TestTriangulo(unittest.TestCase):
         valor_actual = evaluar(3.9, 6.0, 1.2)
         self.assertEqual(valor_esperado, valor_actual)
     
-    # TODO: Agrega tus otros casos de prueba aquí
-    
+    def iso(self):
+        valor_esperado = "El triángulo es isósceles"
+        valor_actual = evaluar(1.9, 2, 2)
+        self.assertEqual(valor_esperado, valor_actual)
+    def esca(self):
+        valor_esperado = "El triángulo es escaleno"
+        valor_actual = evaluar(3, 5, 4)
+        self.assertEqual(valor_esperado, valor_actual)
+    def equi(self):
+        valor_esperado = "El triángulo es equilátero"
+        valor_actual = evaluar(5, 5, 5)
+        self.assertEqual(valor_esperado, valor_actual)
 
 if __name__ == '__main__':
     unittest.main()
